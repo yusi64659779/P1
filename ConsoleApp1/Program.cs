09840ec4d2d8f0d98ea95a298989924f30dc89ec
yusiwei <yusiwei@mail.usf.edu>
2019-09-10 19:56:53 -0400

    P1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,16 +7,15 @@ namespace Practice1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("How many years experance do you have in professional programming");
-            string input = Console.ReadLine();
-            int year = Convert.ToInt32(input);
+            Console.WriteLine("How many years experance do you have in professional programming: ");
+            int year = Convert.ToInt32(Console.ReadLine());
             if (year == 0)
                 Console.WriteLine("Need lots of practice");
-            if (year == 1)
+            else if (year == 1)
                 Console.WriteLine("Looks like you have a liitle experience");
-            if (year == 2)
+            else if (year == 2)
                 Console.WriteLine("Wow! You have been doing this fro a little while");
-            if (year > 2)
+            else
                 Console.WriteLine("You are an expert!");
         }
 
